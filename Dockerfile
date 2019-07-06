c2dcc179c50681cee00976ccb197142eb481a445
Milan Das <milan@ulap.co>
2019-07-06 11:01:21 -0400

    first commit

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# Dockerfile - this is a comment. Delete me if you want.
+FROM docker.io/dmilan/python-3.7-alpine-flask-restplus
+#FROM python:3.7
+
+MAINTAINER Milan Das "milan.das77@gmail.com"
+ENV APP_HOME /app/docker-testing-flask
+#ENV APP_HOME /app
+COPY . $APP_HOME
+WORKDIR $APP_HOME
+#RUN pip install -r requirements.txt
+ENV PYTHONPATH=$PYTHONPATH:$APP_HOME
+
+ENTRYPOINT ["python"]
+CMD ["app/main.py"]
---
+++ app/main.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_restplus import Resource, Api
+import socket, os
+import fcntl
+import struct
+
+
+
+app = Flask(__name__)
+api = Api(app)
+
+@api.route('/ping')
+class PingWorld(Resource):
+    def get(self):
+        host_name = socket.gethostname()
+        host_ip = socket.gethostbyname(host_name)
+
+        outPut={
+            'hostname': host_name,
+            'host_ip': host_ip
+        }
+
+
+
+        return outPut
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port="8080",debug=True)
